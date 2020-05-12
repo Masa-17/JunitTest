c45dc10345ca98f8108128f516fadc8627ca4eda
nor <kubo@heisei-intelligence.com>
2020-05-12 09:34:13 +0900

    階層の変更

---
+++ src/main/java/gradleProject/Sample.java
@@ -0,0 +1,14 @@
+package gradleProject;
+
+public class Sample {
+
+	public int sum(int x , int y) {
+		return x + y;
+	}
+	public int ave(int x , int y) {
+		return x / y;
+	}
+	public int kake(int x , int y) {
+		return x * y;
+	}
+}
